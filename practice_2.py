e986b0bca9a670afd1c373b8f96c8407e0f5193e
angel-ayaya <108294233+angel-ayaya@users.noreply.github.com>
2023-11-03 14:56:43 -0700

    Upload Practices
    Upload first two practices

---
+++ practice_2.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+# create a window
+window = tk.Tk()
+window.title("Windows and Widgets")
+window.geometry("800x500")
+
+# create widgets
+tk.Text()
+
+# run
+window.mainloop()
